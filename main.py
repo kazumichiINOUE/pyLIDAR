4908db43a86c5b3e6ac6d1acd5ec31c95341dae3
KazumichiINOUE <k.inoue@oyama-ct.ac.jp>
2023-12-27 20:12:58 +0900

    refactor: urg をクラス化

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import math
 import sys
 import matplotlib.pyplot as plt
 from utils import *
+from urg_class import Urg
+from common import *
 
-DEBUG_MODE = False
 XMIN = -3000 #[mm]
 XMAX =  3000 #[mm]
 YMIN = -3000 #[mm]
@@ -15,53 +16,11 @@ YMAX =  3000 #[mm]
 plt.figure(figsize=(8, 8))
 
 try:
-    # 通信準備
-    device_file = '/dev/cu.usbmodem1101'
-    try:
-        ser = serial.Serial(device_file, 115200, timeout=1)
-    except serial.SerialException as e:
-        print(f"Connection error {e}", file=sys.stderr)
-        print("Bye")
-        sys.exit(0)
-    
-    time.sleep(2)
-    
-    # VVコマンドを送信し通信テスト
-    success, response = cmd_VV(ser)
-    if success == True:
-        print("[OK] VV")
-        if DEBUG_MODE:
-            print(response, file=sys.stderr)
-    else:
-        print("[False] VV", file=sys.stderr)
-    
-    time.sleep(1)
-    
-    # PPコマンドを送信し通信テスト
-    success, response = cmd_PP(ser)
-    if success == True:
-        print("[OK] PP")
-        if DEBUG_MODE:
-            print(response, file=sys.stderr)
-    else:
-        print("[False] PP", file=sys.stderr)
-    
-    time.sleep(1)
-    
-    # IIコマンドを送信し通信テスト
-    success, response = cmd_II(ser)
-    if success == True:
-        print("[OK] II")
-        if DEBUG_MODE:
-            print(response, file=sys.stderr)
-    else:
-        print("[False] II", file=sys.stderr)
-
-    print("Connect test all clear.")
+    urg = Urg('/dev/cu.usbmodem1101', 115200)
     
     count = 0
     while True:
-        success, urg_data = one_shot(ser)
+        success, urg_data = urg.one_shot()
         if success == True:
             x = []
             y = []
@@ -81,12 +40,12 @@ try:
         else:
             print("False", file=sys.stderr)
     # シリアル接続を閉じる
-    ser.close()
+    urg.close()
     
 except KeyboardInterrupt:
     # Ctrl-Cの後処理
     print("Pressed Ctrl-C")
     # シリアル接続を閉じる
-    ser.close()
+    urg.close()
     print("Bye")
 
---
+++ urg_class.py
@@ -0,0 +1,87 @@
+import serial
+import sys
+import time
+from utils import *
+from common import *
+
+class Urg:
+    def __init__(self, device_file, baurate):
+        self.device_file = device_file
+        self.baurate = baurate
+        self.timeout = 1
+        try:
+            self.ser = serial.Serial(self.device_file, self.baurate, timeout=self.timeout)
+        except serial.SerialException as e:
+            print(f"Connection error {e}", file=sys.stderr)
+            print("Bye")
+            sys.exit(0)
+        time.sleep(2)
+
+        # VVコマンドを送信し通信テスト
+        success, response = cmd_VV(self.ser)
+        if success == True:
+            print("[OK] VV")
+            if DEBUG_MODE:
+                print(response, file=sys.stderr)
+        else:
+            print("[False] VV", file=sys.stderr)
+            sys.exit(0)
+    
+        time.sleep(1)
+    
+        # PPコマンドを送信し通信テスト
+        success, response = cmd_PP(self.ser)
+        if success == True:
+            print("[OK] PP")
+            if DEBUG_MODE:
+                print(response, file=sys.stderr)
+        else:
+            print("[False] PP", file=sys.stderr)
+            sys.exit(0)
+        
+        time.sleep(1)
+        
+        # IIコマンドを送信し通信テスト
+        success, response = cmd_II(self.ser)
+        if success == True:
+            print("[OK] II")
+            if DEBUG_MODE:
+                print(response, file=sys.stderr)
+        else:
+            print("[False] II", file=sys.stderr)
+            sys.exit(0)
+
+        # 通信テストを合格
+        print("Connect test all clear.")
+
+    def one_shot(self):
+        # MDコマンドを送信しone-shot計測
+        success, head, data = cmd_MD(self.ser)
+        urg_data = []
+        if success == True:
+            data_strings = ""
+            for p in data:
+                if len(p) > 0:
+                    check_sum = p[-1]
+                    body = p[:-1]
+                    data_strings += body
+            for i in range(0, len(data_strings), 3):
+                three_chars = data_strings[i:i+3]
+                binary = []
+                for char in three_chars:
+                    ch = ord(char) - 0x30
+                    hex_ascii_value = hex(ch)
+                    bin_val = bin(int(hex_ascii_value, 16)) 
+                    # 2進数を6ビットの2進数に変換
+                    binary.append(format(int(bin_val, 2), '06b'))
+                combined_binary_24bit = ''.join(binary)
+                r = int(combined_binary_24bit, 2)
+                index = int(i/3)
+                urg_data.append((index, r))
+            return True, urg_data
+        else:
+            return False, urg_data
+    
+    def close(self):
+        self.ser.close()
+
