c00f372f94701893c12677f202e48382a0049d7d
KazumichiINOUE <k.inoue@oyama-ct.ac.jp>
2023-12-27 21:21:07 +0900

    refactor: いくつかの共通関数をcommonへ集約

--- utils.py
+++ utils.py
@@ -10,13 +10,6 @@ def remove_semicolon_followed_by_char(line):
         return line[:semicolon_index]
     return line
 
-def index2angle(index):
-    # 以下の数値はUBG-04LX-F01の設定値
-    return (index + 44) * 360/1024 - 135.0
-
-def deg2rad(deg):
-    return deg * math.pi/180.0
-
 def cmd_VV(ser_dev):
     # VVコマンドを送信（デバイス情報を要求）
     ser_dev.write(b'VV\n')
